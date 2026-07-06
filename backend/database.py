c9adcdcb5772db9bd1e61706ea8522779239902a
Marvin <141830291+Marv0407@users.noreply.github.com>
2026-07-06 23:42:25 +0200

    .

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,12 @@
+import os
+
+# Bestimme das absolute Hauptverzeichnis des Projekts (eine Ebene über backend)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, "db.sqlite3")
+
 DATABASE_CONFIG = {
     "connections": {
-        "default": "sqlite://db.sqlite3"
+        "default": f"sqlite://{DB_PATH}"
     },
     "apps": {
         "models": {
