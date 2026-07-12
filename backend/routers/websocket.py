164c437c1f09d174db2928fa672655425081881c
Marvin <141830291+Marv0407@users.noreply.github.com>
2026-07-12 17:29:00 +0200

    Füge Spielmechaniken für Steuern, Zufriedenheit und Gebäudewartung hinzu

--- backend/routers/websocket.py
+++ backend/routers/websocket.py
@@ -5,7 +5,7 @@ from tortoise.exceptions import DoesNotExist
 from tortoise.transactions import in_transaction
 from world_generator import generate_local_sectors
 import asyncio
-from backend import config
+import config
 
 router = APIRouter()
 
